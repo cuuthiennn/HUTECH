da168841cdd948a58d656d046771a7e98ce5e33a
Vũ Văn Thiện <vuthien26203@gmail.com>
2024-08-05 16:56:50 +0700

    Tinh gia tri trung binh

---
+++ src/TinhGiaTriTrungBinh.java
@@ -0,0 +1,27 @@
+import java.time.temporal.ValueRange;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        int tong = 0;
+        float trungBinh;
+        final int soLuong = 5;
+        System.out.println("Nhập vào số nguyên thứ nhất: ");
+        tong += scan.nextInt();
+        scan.nextLine();
+        System.out.println("Nhập vào số nguyên thứ hai: ");
+        tong += scan.nextInt();
+        scan.nextLine();
+        System.out.println("Nhập vào số nguyên thứ ba: ");
+        tong += scan.nextInt();
+        scan.nextLine();
+        System.out.println("Nhập vào số nguyên thứ bốn: ");
+        tong += scan.nextInt();
+        scan.nextLine();
+        System.out.println("Nhập vào số nguyên thứ năm: ");
+        tong += scan.nextInt();
+        trungBinh = 1f * tong / soLuong; Float.
+        System.out.println("Trung bình của năm số nhập là: " + trungBinh);
+    }
+}
